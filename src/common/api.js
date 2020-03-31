47f4523dbb36e2212b7378320b13fb4ada1fa8fc
Leo <memory140662@gmail.com>
2020-04-01 01:04:57 +0800

    移除 變數

--- src/common/api.js
+++ src/common/api.js
@@ -18,7 +18,7 @@ const firebaseConfig = {
     storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
     appId: process.env.REACT_APP_FIREBASE_APP_ID,
-    measurementId: 'G-E922PZY97X',
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 }
 
 firebase.initializeApp(firebaseConfig)
